2688870db378563d188f9aecb5e500fdb41f0bde
devmarsv <gocpman@gmail.com>
2019-03-13 12:26:18 +0900

    깨진파일 수정

--- semiProject/dbscript/MEMBER_SAMPLEDATA.sql
+++ semiProject/dbscript/MEMBER_SAMPLEDATA.sql
@@ -1,35 +1,35 @@
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.nextval, 'admin', 'admin', '관리자', '190315', 'M', '01054380255', 'admin@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '0');
+VALUES ('admin', 'admin', '관리자', '190315', 'M', '01054380255', 'admin@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '0');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.nextval, 'user01', 'pass01', '김태우', '900301', 'M', '01043920295', 'taewoo@naver.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
+VALUES ('user01', 'pass01', '김태우', '900301', 'M', '01043920295', 'taewoo@naver.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.nextval, 'user02', 'pass02', '성우탁', '900302', 'M', '01065839293', 'wootak@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
+VALUES ('user02', 'pass02', '성우탁', '900302', 'M', '01065839293', 'wootak@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.nextval, 'user03', 'pass03', '양석원', '900303', 'M', '01058192939', 'seokwon@naver.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
+VALUES ('MEM', 'user03', 'pass03', '양석원', '900303', 'M', '01058192939', 'seokwon@naver.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.nextval, 'user04', 'pass04', '차우리', '900304', 'F', '01034910293', 'woori@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
+VALUES ('MEM', 'user04', 'pass04', '차우리', '900304', 'F', '01034910293', 'woori@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.nextval, 'user05', 'pass05', '허지욱', '900305', 'F', '01041341234', 'JIWOOK@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
+VALUES ('MEM', 'user05', 'pass05', '허지욱', '900305', 'F', '01041341234', 'JIWOOK@gmail.com', NULL, NULL, DEFAULT, DEFAULT, DEFAULT, '1');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.nextval, 'hongod91', 'tjdwls', '홍성진', '910123', 'F', '01054380256', 'hongod91@gmail.com', '경기도 구리시 교문동 원일아파트 5동 103호', NULL, DEFAULT, DEFAULT, DEFAULT, '2');
+VALUES ('MEM', 'hongod91', 'tjdwls', '홍성진', '910123', 'F', '01054380256', 'hongod91@gmail.com', '경기도 구리시 교문동 원일아파트 5동 103호', NULL, DEFAULT, DEFAULT, DEFAULT, '2');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.NEXTVAL, 'kim', 'kim', '김유정', '981225', 'F', '01023334204', 'kimuser@daum.net', '서울시 강남구 논현로105길 51, 2층 203호', NULL, DEFAULT, DEFAULT, DEFAULT, '2');
+VALUES ('kim', 'kim', '김유정', '981225', 'F', '01023334204', 'kimuser@daum.net', '서울시 강남구 논현로105길 51, 2층 203호', NULL, DEFAULT, DEFAULT, DEFAULT, '2');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.NEXTVAL, 'lee', 'lee', '이정재', '600115', 'M', '01030417777', 'leeuser@naver.com', '경기도 성남시 분당구 정자일로 248, 1301호', NULL, DEFAULT, DEFAULT, DEFAULT, '3');
+VALUES ('lee', 'lee', '이정재', '600115', 'M', '01030417777', 'leeuser@naver.com', '경기도 성남시 분당구 정자일로 248, 1301호', NULL, DEFAULT, DEFAULT, DEFAULT, '3');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.NEXTVAL, 'CHOI', 'CHOI', '최유진', '700430', 'M', '01075839999', 'choiyoujin@gmail.com', '서울시 도봉구 우이천로 328, 103동 1501호', NULL, DEFAULT, DEFAULT, DEFAULT, '3');
+VALUES ('CHOI', 'CHOI', '최유진', '700430', 'M', '01075839999', 'choiyoujin@gmail.com', '서울시 도봉구 우이천로 328, 103동 1501호', NULL, DEFAULT, DEFAULT, DEFAULT, '3');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.NEXTVAL, 'HAN', 'HAN', '한지민', '801130', 'F', '01063234599', 'hanjimin@gmail.com', '서울 강동구 고덕로62길 76, 3동 1002호', NULL, DEFAULT, DEFAULT, DEFAULT, '4');
+VALUES ('HAN', 'HAN', '한지민', '801130', 'F', '01063234599', 'hanjimin@gmail.com', '서울 강동구 고덕로62길 76, 3동 1002호', NULL, DEFAULT, DEFAULT, DEFAULT, '4');
 
 INSERT INTO MEMBER
-VALUES ('MEM'||SEQ_MEMCODE.NEXTVAL, 'JEONG', 'JEONG', '정우셩', '781222', 'M', '01022222222', 'jeongwoosung@gmail.com', '경기도 부천시 신흥로276번길 15, 106동 302호', NULL, DEFAULT, DEFAULT, DEFAULT, '4');
+VALUES ('JEONG', 'JEONG', '정우셩', '781222', 'M', '01022222222', 'jeongwoosung@gmail.com', '경기도 부천시 신흥로276번길 15, 106동 302호', NULL, DEFAULT, DEFAULT, DEFAULT, '4');
