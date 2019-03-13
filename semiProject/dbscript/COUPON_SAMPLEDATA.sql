6ef5310283a8a6d6144ca7b14fe401db05c68f4b
devWoori <woori1373@gmail.com>
2019-03-13 10:34:38 +0900

    DB script 쿠폰, QNA 샘플데이터
    쿠폰, QNA 테이블 DB 샘플데이터 script 입니다.

---
+++ semiProject/dbscript/COUPON_SAMPLEDATA.sql
@@ -0,0 +1,29 @@
+insert into coupon values('coupon'||seq_couponno.nextval,
+'오픈 기념 5% 쿠폰', default, default, '10', '전 상품 적용 가능', 'user04');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'골드 회원 10% 쿠폰', default, '190331', '10', '전 상품 적용 가능', 'user04');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'고객감사 30% 쿠폰', default, '190331', '30', '전 상품 적용 가능', 'user04');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'나라사랑 31% 쿠폰', '190301', '190309', '31', '전 상품 적용 가능', 'user04');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'살랑사랑 봄 쿠폰', default, default, '3', '전 상품 적용 가능', 'user04', 'Y');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'오픈 기념 5% 쿠폰', default, default, '10', '전 상품 적용 가능', 'user01', 'Y');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'골드 회원 10% 쿠폰', default, '190331', '10', '전 상품 적용 가능', 'user02');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'고객감사 30% 쿠폰', default, '190331', '30', '전 상품 적용 가능', 'user03');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'나라사랑 31% 쿠폰', '190301', '190309', '31', '전 상품 적용 가능', 'user01');
+
+insert into coupon values('coupon'||seq_couponno.nextval,
+'살랑사랑 봄 쿠폰', default, default, '3', '전 상품 적용 가능', 'user04', 'Y');
